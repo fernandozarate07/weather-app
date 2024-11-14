65114a884adf839e52352f0f061e0e0ed1dc3609
Fernando Zárate <150300719+fernandozarate07@users.noreply.github.com>
2024-11-14 19:41:13 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles/general.css';
+import './styles/styles.css';
+
+console.log("Webpack Template Initialized!");
---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html', // Archivo HTML base
+    }),
+  ],
+};
